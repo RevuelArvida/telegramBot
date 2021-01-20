72e7eaf7acc5926aca196be3d4710f1fc5bcb8d5
egoza <egoza98@gmail.com>
2021-01-21 01:00:04 +0300

    Added startup and Keyboard

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,18 +1,27 @@
-
-import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import lombok.RequiredArgsConstructor;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot extends TelegramLongPollingCommandBot {
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class Bot extends TelegramLongPollingBot {
 
     private final String botName;
     private final String token;
 
-    Bot (String botName, String token){
-        this.botName = botName;
-        this.token = token;
+    private Bot(){
+        botName = "botName";
+        token = "token";
     }
+
 
     @Override
     public String getBotUsername() {
@@ -20,19 +29,51 @@ public class Bot extends TelegramLongPollingCommandBot {
     }
 
     @Override
-    public void processNonCommandUpdate(Update update) {
-        Message msg = update.getMessage();
-        Long chatId = update.getMessage().getChatId();
-
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId.toString());
-        sendMessage.setText(msg.getText());
-    }
-
-    @Override
     public String getBotToken() {
         return token;
     }
 
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message msg = update.getMessage();
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Привет, " + msg.getChat().getFirstName() + ", я бот анекдотчик! Знаю все анекдоты про Штирлица! Хочешь расскажу?");
+        } else {
+            sendMsg(msg, "Я еще ничего не умею!");
+        }
+    }
+
+    private void sendMsg(Message msg, String text){
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId().toString());
+        s.setText(text);
+
+        setButtons(s);
+
+        try {
+            execute(s);
+        } catch (TelegramApiException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(keyboardMarkup);
+        keyboardMarkup.setSelective(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow firstRow = new KeyboardRow();
+        KeyboardRow secondRow = new KeyboardRow();
+        firstRow.add(new KeyboardButton("Вкинь анек"));
+        secondRow.add(new KeyboardButton("Найди мне анек"));
+        keyboard.add(firstRow);
+        keyboard.add(secondRow);
+
+        keyboardMarkup.setKeyboard(keyboard);
+    }
 }
 
---
+++ src/main/java/Keyboard.java
@@ -0,0 +1,10 @@
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
+public class Keyboard {
+
+    public Keyboard() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+    }
+
+}
